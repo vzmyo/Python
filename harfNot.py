45a81dbae8f8789648d7e6057d199ee3aff4c1f1
Ömer SEVİNÇ <sevinc.omer@gmail.com>
2021-11-16 16:47:18 +0200

    harfnot

---
+++ harfNot.py
@@ -0,0 +1,21 @@
+def harfNot(dersNot):
+    if dersNot >= 80:
+        print("A")
+    elif dersNot >= 70:
+        print("B")
+    elif dersNot >= 60:
+        print("C")
+    elif dersNot >= 40:
+        print("D")
+    elif dersNot >= 10:
+        print("F")
+    else:
+        print("FF")
+
+harfNot(4)
+harfNot(34)
+harfNot(44)
+harfNot(64)
+harfNot(74)
+harfNot(84)
+        
